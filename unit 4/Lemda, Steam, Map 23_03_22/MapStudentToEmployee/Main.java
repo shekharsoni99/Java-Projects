0facc19812411957cf5a00d8f802945d2e3b3fe8
shekharsoni99 <shekharsoni99@gmail.com>
2022-03-28 03:01:08 +0530

    Stream Map

--- unit 4/Lemda, Steam, Map 23_03_22/MapStudentToEmployee/Main.java
+++ unit 4/Lemda, Steam, Map 23_03_22/MapStudentToEmployee/Main.java
@@ -17,17 +17,21 @@ public class Main {
 
         List<Employee> employeeList = new ArrayList<>();
 
-        for (Student s:studentList) {
-//            System.out.println(s);
-            employeeList.add(new Employee(s.getRoll(), s.getName(), s.getMarks()*1000));
-        }
+//        for (Student s:studentList) {
+//            employeeList.add(new Employee(s.getRoll(), s.getName(), s.getMarks()*1000));
+//        }
 
-//        Stream<Student> employeeStream = studentList.stream();
+        Stream<Student> studentStream = studentList.stream();
 
-//        employeeStream.forEach(i -> {
+
+        List employeeList1 = studentStream.map(s -> new Employee(s.getRoll(), s.getName(), s.getMarks()*1000)).collect(Collectors.toList());
+        System.out.println(employeeList1);
+
+        employeeList1.forEach(student -> System.out.println(student));
+//        studentStream.forEach(i -> {
 //            employeeList.add(new Employee(i.getRoll(), i.getName(), i.getMarks()*1000));
 //        });
 
-        System.out.println(employeeList);
+//        employeeList.forEach(i -> System.out.println(i));
     }
 }
