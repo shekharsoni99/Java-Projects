25683a5f156a46f6f1b5d1e81e03e901db14da6d
Shekhar Soni <94068631+shekharsoni99@users.noreply.github.com>
2022-03-28 11:06:29 +0530

    Update Main.java

--- unit 4/Evaluation 3/SortMap/Main.java
+++ unit 4/Evaluation 3/SortMap/Main.java
@@ -5,12 +5,18 @@ import java.util.*;
 
 public class Main {
 
-    public Map<String, Employee> getSortedMapWithValue(Map<String, Employee>  Comparator(Employee e1, Employee e2) -> {
-
-    });
-
     public static void main(String[] args) {
+        
+        HashMap<String, Employee> employeeHashMap = new HashMap<>();
+        
+        employeeHashMap.put("HR", new Employee("1324", "Ramesh", 65439));
+        employeeHashMap.put("Sales", new Employee("5436", "Ram", 55409));
+        employeeHashMap.put("Marketing", new Employee("765", "Shyam", 76579));
+        employeeHashMap.put("Accounts", new Employee("876", "Rakesh", 45357));
+        
+        
 
     }
 }
+
 
