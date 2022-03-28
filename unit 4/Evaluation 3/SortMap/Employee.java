e93835088177ab794cd601444ee85f10819f3b6a
Shekhar Soni <94068631+shekharsoni99@users.noreply.github.com>
2022-03-28 11:06:51 +0530

    Update Employee.java

--- unit 4/Evaluation 3/SortMap/Employee.java
+++ unit 4/Evaluation 3/SortMap/Employee.java
@@ -5,5 +5,42 @@ public class Employee {
     private String empName;
     private double salary;
 
+    public Employee(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+    }
 
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId='" + empId + '\'' +
+                ", empName='" + empName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
